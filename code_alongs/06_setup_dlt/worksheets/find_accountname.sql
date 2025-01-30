608d1ff1499aa33fe00806acc5c1d8ff057a6738
Jie Hou <namehb@gmail.com>
2025-01-30 20:55:46 +0100

    setup dlt

---
+++ code_alongs/06_setup_dlt/worksheets/find_accountname.sql
@@ -0,0 +1,27 @@
+use role orgadmin;   -- 自定义role 权限高
+show accounts;       -- 列出当前 Snowflake 组织下的所有 帐户
+
+
+-- 在accounts 里找这个，然后把 wy59321.west-europe.azure 添加到 screts.toml 文档。
+-- account_locator_url
+-- https://wy59321.west-europe.azure.snowflakecomputing.com
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
