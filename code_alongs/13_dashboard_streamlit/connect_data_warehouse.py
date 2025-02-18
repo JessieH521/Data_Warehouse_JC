b36e2dec6a60d5b8e0eda89fc6c3877fae723746
Jie Hou <namehb@gmail.com>
2025-02-18 20:40:11 +0100

    13_dashboard_streamlit

---
+++ code_alongs/13_dashboard_streamlit/connect_data_warehouse.py
@@ -0,0 +1,29 @@
+# connect to the warehouse
+import os
+from dotenv import load_dotenv
+from snowflake.connector import connect
+import pandas as pd
+
+# connect to the mark layer
+def query_job_listings(query = 'select * from mart_job_listings'):
+    load_dotenv()
+
+    with connect(
+        user=os.getenv("SNOWFLAKE_USER"),
+        password=os.getenv("SNOWFLAKE_PASSWORD"),
+        account=os.getenv("SNOWFLAKE_ACCOUNT"),
+        warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
+        database=os.getenv("SNOWFLAKE_DATABASE"),
+        schema=os.getenv("SNOWFLAKE_SCHEMA"),
+        role=os.getenv("SNOWFLAKE_ROLE"),
+    ) as conn:
+        
+        # Execute the query
+        df = pd.read_sql(query, conn)
+        return df
+
+print(query_job_listings())
+
+
+
+
